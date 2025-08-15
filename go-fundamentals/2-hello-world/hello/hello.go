c3ee1d32b49929d94e422fb1e6aaf3cb51f70a57
Toan <bachmatoan2910@gmail.com>
2025-08-15 19:03:04 -0400

    hello.go: further refactored the Hello(...) with a new greetingPrefix(...)

--- go-fundamentals/2-hello-world/hello/hello.go
+++ go-fundamentals/2-hello-world/hello/hello.go
@@ -10,14 +10,18 @@ func Hello(name string, language string) string {
 	if len(name) == 0 {
 		return "World"
 	}
-	prefix := englishHelloPrefix
 
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case spanish:
 		prefix = spanishHelloPrefix
 	case french:
 		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-
-	return prefix + name
+	return
 }
