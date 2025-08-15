0e57f1bf2c94e6282ef15854c7752fc5a8e2fc5f
Toan <bachmatoan2910@gmail.com>
2025-08-15 18:34:16 -0400

    Go Subtest and Go's doc

--- go-fundamentals/2-hello-world/READMD.md
+++ go-fundamentals/2-hello-world/READMD.md
@@ -4,3 +4,13 @@ You need to satisfy a few requirements below to write a test in Go:
 - A test function must start with `Test`
 - A test function must take only one argument `t *testing.T`
 - To use the `*testing.T` type, you need to `import "testing"`, like we did with `fmt` in other file.
+- Do `t.Fail()` when you want to fail
+
+# Go's documentation
+There are a few options to see documentation:
+1. go doc [something]
+2. pkgsite -open .
+    - prerequisite:  `go install golang.org/x/pkgsite/cmd/pkgsite@latest` 
+
+# Go Subtest
+Use `t.Run(...)` to run subtests in Go
--- go-fundamentals/2-hello-world/hello/hello_test.go
+++ go-fundamentals/2-hello-world/hello/hello_test.go
@@ -2,14 +2,31 @@ package hello
 
 import "testing"
 
-func Hello() string {
-	return "Hello, world\n"
+const englishHelloPrefix = "Hello, "
+
+func Hello(name string) string {
+	if len(name) == 0 {
+		return "Hello, World"
+	}
+	return englishHelloPrefix + name
 }
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world\n"
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
