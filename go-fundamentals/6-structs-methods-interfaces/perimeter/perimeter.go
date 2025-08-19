918a374058ad7ef0145155bba7e8d78d96238505
Toan <bachmatoan2910@gmail.com>
2025-08-19 16:17:59 -0400

    perimeter.go: added Rectangle struct {}

--- go-fundamentals/6-structs-methods-interfaces/perimeter/perimeter.go
+++ go-fundamentals/6-structs-methods-interfaces/perimeter/perimeter.go
@@ -1,5 +1,14 @@
 package perimeter
 
-func Perimeter(width, height float64) float64 {
-	return 2 * (width + height)
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r *Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
+
+func (r *Rectangle) Area() float64 {
+	return r.Width * r.Height
 }
