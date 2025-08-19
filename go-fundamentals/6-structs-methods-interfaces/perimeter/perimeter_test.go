d51784dadf325fdd99fd1b12285fe515a168c353
Toan <bachmatoan2910@gmail.com>
2025-08-19 16:18:45 -0400

    perimeter_test.go: modified TestPerimeter and TestArea to be aligned with perimeter.go

--- go-fundamentals/6-structs-methods-interfaces/perimeter/perimeter_test.go
+++ go-fundamentals/6-structs-methods-interfaces/perimeter/perimeter_test.go
@@ -3,7 +3,8 @@ package perimeter
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rec := Rectangle{10.0, 10.0}
+	got := rec.Perimeter()
 	want := 40.0
 
 	if got != want {
@@ -12,7 +13,8 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(12.0, 6.0)
+	rec := Rectangle{12.0, 6.0}
+	got := rec.Area()
 	want := 72.0
 
 	if got != want {
