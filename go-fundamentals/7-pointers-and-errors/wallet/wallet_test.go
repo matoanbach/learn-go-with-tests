f5a1aff7f963fd7547914743b9b8646f0898735d
Toan <bachmatoan2910@gmail.com>
2025-08-19 18:39:42 -0400

    wallet_test.go: added TestWallet()

--- go-fundamentals/7-pointers-and-errors/wallet/wallet_test.go
+++ go-fundamentals/7-pointers-and-errors/wallet/wallet_test.go
@@ -1,14 +1,17 @@
 package wallet
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
 	wallet.Deposit(10)
 
 	got := wallet.Balance()
-	want := 10
-
+	want := Bitcoint(10)
+	fmt.Printf("address of my wallet in test is %p \n", &wallet)
 	if got != want {
 		t.Errorf("got %d want %d", got, want)
 	}
