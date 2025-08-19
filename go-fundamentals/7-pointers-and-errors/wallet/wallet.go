ce2bd9d2486969ad12d9b718be1e7de9c9f25730
Toan <bachmatoan2910@gmail.com>
2025-08-19 18:40:12 -0400

    wallet.go: added Stringer, Bitcoint, and Wallet Struct

--- go-fundamentals/7-pointers-and-errors/wallet/wallet.go
+++ go-fundamentals/7-pointers-and-errors/wallet/wallet.go
@@ -1,13 +1,26 @@
 package wallet
 
-type Wallet struct {
-	balance int
+import "fmt"
+
+type Bitcoint int
+
+type Stringer interface {
+	String() string
 }
 
-func (w Wallet) Deposit(amount int) {
+func (b Bitcoint) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoint
+}
+
+func (w *Wallet) Deposit(amount Bitcoint) {
+	fmt.Printf("address of my wallet in wallet.go is %p \n", w)
 	w.balance += amount
 }
 
-func (w Wallet) Balance() int {
+func (w Wallet) Balance() Bitcoint {
 	return w.balance
 }
