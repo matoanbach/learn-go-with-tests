fa8bf0d37846e14fa9bd412ee2f70c18945aa745
Toan <bachmatoan2910@gmail.com>
2025-08-21 18:17:13 -0400

    mocking_test.go: TestCountdown() with a new iteration - sleep before every print

--- go-fundamentals/10-mocking/mocking/mocking_test.go
+++ go-fundamentals/10-mocking/mocking/mocking_test.go
@@ -2,6 +2,7 @@ package mocking
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
@@ -30,17 +31,37 @@ const write = "write"
 const sleep = "sleep"
 
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	spySleeper := &SpySleeper{}
-	Countdown(buffer, spySleeper)
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		Countdown(buffer, &SpyCountdownOperations{})
 
-	got := buffer.String()
-	want := `3
+		got := buffer.String()
+		want := `3
 2
 1
 Go!`
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+		}
+	})
 }
