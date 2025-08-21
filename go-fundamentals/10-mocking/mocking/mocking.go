f49824ba133d9088dcf4222cd39396f1571afe13
Toan <bachmatoan2910@gmail.com>
2025-08-21 18:07:01 -0400

    mocking.go: added Sleeper interface and RealSleep struct

--- go-fundamentals/10-mocking/mocking/mocking.go
+++ go-fundamentals/10-mocking/mocking/mocking.go
@@ -3,10 +3,27 @@ package mocking
 import (
 	"fmt"
 	"io"
+	"time"
 )
 
-func Countdown(w io.Writer) {
-	for _, s := range []string{"Print ", "3, ", "2, ", "1 ", "and ", "Go!"} {
-		fmt.Fprintf(w, "%s", s)
+const finalWord = "Go!"
+const countdownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+type RealSleep struct {
+}
+
+func (r RealSleep) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(w io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(w, i)
+		sleeper.Sleep()
 	}
+	fmt.Fprint(w, finalWord)
 }
