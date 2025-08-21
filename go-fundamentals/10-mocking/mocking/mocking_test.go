2810d55538cba73cbf5e9295bf327595107dd010
Toan <bachmatoan2910@gmail.com>
2025-08-21 18:11:55 -0400

    mocking_test.go: SpyCountdownOperations

--- go-fundamentals/10-mocking/mocking/mocking_test.go
+++ go-fundamentals/10-mocking/mocking/mocking_test.go
@@ -13,6 +13,22 @@ func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
 
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
+
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
 	spySleeper := &SpySleeper{}
