1eb8890de17e45bb26f97bca657c159e90124a27
Toan <bachmatoan2910@gmail.com>
2025-08-21 15:00:31 -0400

    mymap_test.go: TestSearch with 2 cases, known and unknown key

--- go-fundamentals/8-maps/mymap/mymap.go
+++ go-fundamentals/8-maps/mymap/mymap.go
@@ -1,8 +1,13 @@
 package mymap
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(key string) string {
-	val, _ := d[key]
-	return val
+func (d Dictionary) Search(key string) (string, error) {
+	val, ok := d[key]
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+	return val, nil
 }
--- go-fundamentals/8-maps/mymap/mymap_test.go
+++ go-fundamentals/8-maps/mymap/mymap_test.go
@@ -5,9 +5,20 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
-	assertString(t, got, want)
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+		assertString(t, got, want)
+	})
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknow")
+		got := err.Error()
+		want := "could not find the word you were looking for"
+		if err == nil {
+			t.Fatalf("expected to get an error")
+		}
+		assertString(t, got, want)
+	})
 }
 
 func assertString(t testing.TB, got, want string) {
